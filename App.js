0cca965ac877978d6a25104fa1e323415c48fd7a
kidduts <dinuionutvladut99@gmail.com>
2021-05-01 15:51:44 +0300

    added server in flask and user services

--- App.js
+++ App.js
@@ -11,38 +11,13 @@ import {
 
 import { Asset } from "expo-asset";
 import { AppLoading } from "expo";
-const REGISTER_URL = 'http://127.0.0.1:5001/register_user';
+
 export default class App extends React.Component {
   state = {
     email: "",
     password: "",
   };
-
-  userRegister = () =>{
-    const {username} = "name";
-		const {email} = this.state;
-		const {password} = this.state;
-    var json = {
-        'username': "username",
-        'email' : email,
-        'password' : password
-    }
-    
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", REGISTER_URL);
-
-    xhr.setRequestHeader("Content-Type", "application/json");
-
-    xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-        console.log(xhr.status);
-        console.log(xhr.responseText);
-    }};
-
-    xhr.send(JSON.stringify(json));
-  }
   
-
   render() {
     return (
       <View style={styles.container}>
